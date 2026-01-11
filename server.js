66a1a741470de11cf065969e29dc2e27dd8e77c8
maiselamadise <maisela.mad@gmail.com>
2026-01-12 01:30:29 +0200

    Edit

--- server.js
+++ server.js
@@ -2,14 +2,15 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
+require("dotenv").config()
 const app = express()
-const static = require("./routes/static")
+const staticRoutes = require("./routes/static")
 
 /* ***********************
  * View Engine and Templates
@@ -21,16 +22,16 @@ app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
-app.use(static)
-app.get("/", function (req, res) {
-  res.render("index", {title: "Home"})
-}
+app.use(staticRoutes)
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-
 const port = process.env.PORT
 const host = process.env.HOST
 
