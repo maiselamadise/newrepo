ccebd2f358a80944693b5b0cb0ed66f9330d6881
maiselamadise <maisela.mad@gmail.com>
2026-01-30 01:45:21 +0200

    Commit

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,24 +1,17 @@
-// controllers/inventoryController.js
-const invModel = require("../models/inventory-model");
-const utilities = require("../utilities");
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities")
 
-async function buildByInvId(req, res, next) {
-  const invId = req.params.invId;
-  const vehicleData = await invModel.getVehicleById(invId);
+const invController = {}
 
-  if (!vehicleData) {
-    return res.status(404).render("inventory/detail", {
-      title: "Vehicle Not Found",
-      message: "Sorry, we couldn't find that vehicle."
-    });
-  }
-
-  const vehicleHTML = utilities.buildVehicleDetailHTML(vehicleData);
-
+invController.buildInventoryDetail = async function (req, res) {
+  const invId = req.params.invId
+  const vehicle = await invModel.getInventoryById(invId)
+  const nav = await utilities.getNav()
   res.render("inventory/detail", {
-    title: `${vehicleData.make} ${vehicleData.model}`,
-    vehicleHTML
-  });
+    title: `${vehicle.inv_make} ${vehicle.inv_model}`,
+    nav,
+    vehicle,
+  })
 }
 
-module.exports = { buildByInvId };
+module.exports = invController
