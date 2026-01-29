50223bd0f4f83253242c5bea9bd8f44f1ba2dd99
maiselamadise <maisela.mad@gmail.com>
2026-01-30 00:25:31 +0200

    Commit

---
+++ routes/indexRoute.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = new express.Router()
+const baseController = require("../controllers/baseController")
+
+router.get("/", baseController.buildHome)
+
+module.exports = router
