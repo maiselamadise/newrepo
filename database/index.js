8411e3f8e92f85b178ad95645e1d76158a925264
maiselamadise <maisela.mad@gmail.com>
2026-01-29 23:53:04 +0200

    Commit

--- database/index.js
+++ database/index.js
@@ -1,26 +1,24 @@
-const {pool}  = require("pg");
-require("dotenv").config();
+const { Pool } = require("pg")
+require("dotenv").config()
+
 /********* Database Connection *********/
-let pool;
+let pool
+
 if (process.env.NODE_ENV === "production") {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
-  });
+  })
+} else {
+  // local development
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: false,
+  })
 }
 
-// For troubleshooting locally
-//guring development
 module.exports = {
-    async query(text, params) {
-        try {
-            const res = await pool.query(text, params);
-            return res;
-        } catch (err) {
-            console.error('Database query error:', err);
-            throw err;
-        }
-    },
-};
+  query: (text, params) => pool.query(text, params),
+}
