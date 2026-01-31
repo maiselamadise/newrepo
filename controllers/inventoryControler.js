0066de92f732f7c2630c4b6db2474b52ebabd252
maiselamadise <maisela.mad@gmail.com>
2026-01-31 17:29:58 +0200

    commit

---
+++ controllers/inventoryControler.js
@@ -0,0 +1,11 @@
+const invController = {}
+
+invController.buildInventoryDetail = async function (req, res, next) {
+  try {
+    // logic
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports = invController
