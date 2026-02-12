837d1d0dca0178acacd0b6827c2d51cb5ee27390
maiselamadise <maisela.mad@gmail.com>
2026-02-12 09:45:50 +0200

    commit

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,9 +1,34 @@
 const utilities = require("../utilities/")
 const baseController = {}
 
-baseController.buildHome = async function(req, res){
+baseController.buildHome = async (req, res) => {
   const nav = await utilities.getNav()
-  res.render("index", {title: "Home", nav})
+  res.render("index", { title: "Home", nav })
+}
+
+baseController.buildCustom = async (req, res) => {
+  const nav = await utilities.getNav()
+  res.render("custom", { title: "Custom Vehicles", nav })
+}
+
+baseController.buildSedan = async (req, res) => {
+  const nav = await utilities.getNav()
+  res.render("sedan", { title: "Sedans", nav })
+}
+
+baseController.buildSuv = async (req, res) => {
+  const nav = await utilities.getNav()
+  res.render("suv", { title: "SUVs", nav })
+}
+
+baseController.buildTruck = async (req, res) => {
+  const nav = await utilities.getNav()
+  res.render("truck", { title: "Trucks", nav })
+}
+
+baseController.buildLogin = async (req, res) => {
+  const nav = await utilities.getNav()
+  res.render("login", { title: "Login", nav })
 }
 
 module.exports = baseController
--- routes/baseRoute.js
+++ routes/baseRoute.js
@@ -9,4 +9,29 @@ router.get(
   utilities.handleErrors(baseController.buildHome)
 )
 
+router.get(
+  "/vehicles/custom",
+  utilities.handleErrors(baseController.buildCustom)
+)
+
+router.get(
+  "/vehicles/sedan",
+  utilities.handleErrors(baseController.buildSedan)
+)
+
+router.get(
+  "/vehicles/suv",
+  utilities.handleErrors(baseController.buildSuv)
+)
+
+router.get(
+  "/vehicles/truck",
+  utilities.handleErrors(baseController.buildTruck)
+)
+
+router.get(
+  "/account/login",
+  utilities.handleErrors(baseController.buildLogin)
+)
+
 module.exports = router
